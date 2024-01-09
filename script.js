f7a1c7b27083d744d3c6de7d671751888b33dc1b
MarceloBiotto <124075281+MarceloBiotto@users.noreply.github.com>
2024-01-09 11:43:46 -0300

    alterando tempo

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos= 10
+    tempoDecorridoEmSegundos= 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
